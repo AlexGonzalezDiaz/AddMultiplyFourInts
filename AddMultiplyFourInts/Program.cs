5b5f100a87160f0a184670e6befca9b9b13dad0b
AlexGonzalezDiaz <138159711+AlexGonzalezDiaz@users.noreply.github.com>
2023-07-06 16:06:47 -0500

    Add project files.

---
+++ AddMultiplyFourInts/Program.cs
@@ -0,0 +1,11 @@
+﻿// A program that initializes four integer variables that provides the sum and product of these four numbers respectively. 
+
+Console.WriteLine("Teacher's Copy");
+int var1 = 13;
+int var2 = 55;
+int var3 = 123;
+int var4 = 325;
+int sum = var1 + var2 + var3 + var4;
+int prod = var1 * var2 * var3 * var4;
+Console.WriteLine(" The sum of {0}, {1}, {2}, and {3} = {4}", var1, var2, var3, var4, sum);
+Console.WriteLine(" The product of {0}, {1}, {2}, and {3} = {4}", var1, var2, var3, var4, prod);
